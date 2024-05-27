b73b33ff86139451831a2139eda998d8546eb0bb
Dragos Tudor <dragos.tudor@gmail.com>
2024-05-27 09:30:04 +0000

    refactor(extensions): remove verification logger funcs.

--- Simple.Shared.Extensions/Loggers/Verifying.cs
+++
@@ -1,9 +0,0 @@
-
-using Microsoft.Extensions.Logging;
-
-namespace Simple.Shared.Extensions;
-
-partial class ExtensionsFuncs
-{
-  public static bool IsErrorLogLevel(LogLevel level) => level == LogLevel.Error;
-}
