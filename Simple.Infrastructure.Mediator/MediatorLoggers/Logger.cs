fa76e8929534b15c00df3a1a66367c477e906490
Dragos Tudor <dragos.tudor@gmail.com>
2024-05-27 08:22:35 +0000

    refactor(mediator): remove logging support.

--- Simple.Infrastructure.Mediator/MediatorLoggers/Logger.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace Simple.Infrastructure.Mediator;
-
-partial class MediatorFuncs
-{
-  public static readonly ILogger Logger = CreateLogger(GetLoggerFactory(), typeof(MediatorFuncs).Namespace!);
-}
--- Simple.Infrastructure.Mediator/Subscribers/Subscriber.cs
+++ Simple.Infrastructure.Mediator/Subscribers/Subscriber.cs
@@ -1,8 +1,4 @@
 
 namespace Simple.Infrastructure.Mediator;
 
-public record Subscriber<TMessage> (
-  string SubscriberId,
-  string MessageType,
-  Func<TMessage, CancellationToken, Task<string?>> MessageHandler
-);
+public record Subscriber<TMessage> (string SubscriberId, string MessageType, Func<TMessage, CancellationToken, Task<string?>> MessageHandler);
