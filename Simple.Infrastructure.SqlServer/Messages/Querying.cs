9b303608f6d69580f9e3ca6351b38a451ec52226
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-04 14:04:18 +0000

    change(sqlserver): add new message queries.

--- Simple.Infrastructure.SqlServer/Messages/Querying.cs
+++ Simple.Infrastructure.SqlServer/Messages/Querying.cs
@@ -3,9 +3,12 @@ namespace Simple.Infrastructure.SqlServer;
 
 partial class SqlServerFuncs
 {
-  public static IQueryable<Message> FindMessageByKey (IQueryable<Message> query, Guid messageId) =>
-    query.Where(contact => contact.MessageId == messageId);
-
   public static IQueryable<Message> FindActiveMessages (IQueryable<Message> query) =>
-    query.Where(contact => contact.IsActive);
+    query.Where(message => message.IsActive);
+
+  public static IQueryable<Message> FindMessageByKey (IQueryable<Message> query, Guid messageId) =>
+    query.Where(message => message.MessageId == messageId);
+
+  public static IQueryable<Message> FindMessageByParent (IQueryable<Message> query, Guid parentId) =>
+    query.Where(message => message.ParentId == parentId);
 }
