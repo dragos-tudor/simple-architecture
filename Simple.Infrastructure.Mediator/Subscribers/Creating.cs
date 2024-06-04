2e4c7ac9c3d20f67633e59945ba8df1fab75474e
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-04 16:00:27 +0000

    change(mediator): hide test heper func.

--- Simple.Infrastructure.Mediator/Subscribers/Creating.cs
+++ Simple.Infrastructure.Mediator/Subscribers/Creating.cs
@@ -3,7 +3,7 @@ namespace Simple.Infrastructure.Mediator;
 
 partial class MediatorFuncs
 {
-  public static Subscriber<TMessage> CreateSubscriber<TMessage> (string subscriberId, Func<TMessage, CancellationToken, Task<string?>> messageHandler)=>
+  internal static Subscriber<TMessage> CreateSubscriber<TMessage> (string subscriberId, Func<TMessage, CancellationToken, Task<string?>> messageHandler)=>
     CreateSubscriber<TMessage, TMessage> (subscriberId, messageHandler);
 
   public static Subscriber<TMessage> CreateSubscriber<TMessage, TPayload> (string subscriberId, Func<TMessage, CancellationToken, Task<string?>> messageHandler)=>
