f649db1f986d6f9893d0117d6eaf7fb63da34faa
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-04 14:14:48 +0000

    refactor(services): reorganize notifications.

---
+++ Simple.Domain.Services/Notifications/AddedToAgendaNotification copy.cs
@@ -0,0 +1,4 @@
+
+namespace Simple.Domain.Services;
+
+public record AddedToAgendaNotification (string AddressTo, string Title, string Body);
---
+++ Simple.Domain.Services/Notifications/Getting.cs
@@ -0,0 +1,7 @@
+
+namespace Simple.Domain.Services;
+
+partial class ServicesFuncs
+{
+  static string GetAddedToAgendaBody (string agendaOwner) => $"You have been added to {agendaOwner}'s agenda";
+}
