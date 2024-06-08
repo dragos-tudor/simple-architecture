c6c575529ac59a51a2820e727d281900d624a279
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-08 16:21:44 +0000

    refactor(api): load servers options from config file.

--- Simple.Web.Api/Program.cs
+++ Simple.Web.Api/Program.cs
@@ -1,53 +1,45 @@
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+app.UseExceptionHandler().UseRouting();
 
 using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(10));
 var cancellationToken = cancellationTokenSource.Token;
 
-var sqlServerOptions = new SqlServerOptions (
-  "sa", "admin.P@ssw0rd",
-  "sqluser", "sqluser.P@ssw0rd",
-  "mcr.microsoft.com/mssql/server:2019-latest", "simple-sql",
-  "agenda-api", "simple-network",
-  1433
-);
-var replicaSetOptions = new MongoReplicaSetOptions (
-  "mongo:4.2.24", ["simple-mongo1", "simple-mongo2", "simple-mongo3"],
-  "simple-network", "rs0",
-  "agenda-api", ["contacts", "messages"],
-  27017
-);
+var sqlServerOptions = builder.Configuration.GetSection(nameof(SqlServerOptions)).Get<SqlServerOptions>()!;
+var replicaSetOptions = builder.Configuration.GetSection(nameof(MongoReplicaSetOptions)).Get<MongoReplicaSetOptions>()!;
+// https://github.com/dotnet/runtime/issues/83803
+replicaSetOptions = replicaSetOptions with { CollNames = replicaSetOptions.CollNames.Distinct().ToArray(), ContainerNames = replicaSetOptions.ContainerNames.Distinct().ToArray() };
+
 await Task.WhenAll(
   InitializeSqlServerAsync(sqlServerOptions, cancellationToken),
   InitializeMongeReplicaSetAsync (replicaSetOptions, cancellationToken)
 );
 
-
-builder.Services.AddProblemDetails();
-var app = builder.Build();
 var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
 var logger = loggerFactory.CreateLogger(nameof(Simple.Web));
 
-var notificationServerOptions = new NotificationServerOptions("localhost", 9025);
+var notificationServerOptions = builder.Configuration.GetSection(nameof(NotificationServerOptions)).Get<NotificationServerOptions>()!;
 var shutdownNotificationServer = StartNotificationServer(notificationServerOptions, (notification) => LogSentNotification(logger, notification.From, notification.To, notification.Title));
 var sendNotification = CreateNotificationSender(notificationServerOptions);
 
-app.UseExceptionHandler().UseRouting();
-
 var sqlMessageQueue = CreateMessageQueue<Message>(1000);
-var sqlConnString = CreateSqlConnectionString("agenda-api", "sqluser", "sqluser.P@ssw0rd", "simple-sql");
+var sqlConnString = CreateSqlConnectionString(sqlServerOptions.DbName, sqlServerOptions.UserName, sqlServerOptions.UserPassword, sqlServerOptions.ContainerName);
 var sqlDbContextFactory = new AgendaContextFactory(CreateSqlContextOptions<AgendaContext>(sqlConnString));
 var sqlSubscribers = RegisterSqlSubscribers(TimeProvider.System, sqlDbContextFactory, sendNotification, sqlMessageQueue, logger);
 _ = ConsumeSqlMessages(sqlMessageQueue, sqlSubscribers, sqlDbContextFactory, logger);
 MapSqlEndpoints(app, sqlDbContextFactory, sqlMessageQueue, logger);
 
 var mongoMessageQueue = CreateMessageQueue<Message>(1000);
-var mongoConnString = GetMongoConnectionString("simple-mongo1,simple-mongo2,simple-mongo3", "rs0");
-var mongoDb = GetMongoDatabase(CreateMongoClient(mongoConnString), "agenda-api");
+var mongoConnString = GetMongoConnectionString(string.Join(",", replicaSetOptions.ContainerNames), replicaSetOptions.ReplicaSet);
+var mongoDb = GetMongoDatabase(CreateMongoClient(mongoConnString), replicaSetOptions.DbName);
 var mongoSubscribers = RegisterMongoSubscribers(TimeProvider.System, mongoDb, sendNotification, mongoMessageQueue, logger);
 _ = ConsumeMongoMessages(mongoMessageQueue, mongoSubscribers, mongoDb, logger);
 MapMongoEndpoints(app, mongoDb, mongoMessageQueue, logger);
--- Simple.Web.Api/ServerEndpoints/Mapping.Mongo.cs
+++ Simple.Web.Api/ServerEndpoints/Mapping.Mongo.cs
@@ -9,7 +9,7 @@ partial class ApiFuncs
 {
   internal static WebApplication MapMongoEndpoints (WebApplication app, IMongoDatabase agendaDb, Channel<Message> messageQueue, ILogger logger)
   {
-    app.MapPost("/mongo/contacts", ([FromForm]Contact contact, HttpContext httpContext) =>
+    app.MapPost("/mongo/contacts", (Contact contact, HttpContext httpContext) =>
       CreateContactMongoEndpoint(contact, agendaDb, messageQueue, httpContext, logger)).DisableAntiforgery();
 
     app.MapPost("/mongo/contacts/{contactId}/phonenumbers", (Guid contactId, [FromForm]PhoneNumber phoneNumber, HttpContext httpContext) =>
