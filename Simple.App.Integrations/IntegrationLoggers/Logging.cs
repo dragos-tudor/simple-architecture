26df9be3b5578c356033aceb5127a386d9ae5a08
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-18 10:27:13 +0000

    refactor(integrations): move serilog sinks references.

--- Simple.App.Integrations/IntegrationLoggers/Logging.cs
+++ Simple.App.Integrations/IntegrationLoggers/Logging.cs
@@ -3,6 +3,6 @@ namespace Simple.App.Integrations;
 
 partial class IntegrationFuncs
 {
-  [LoggerMessage(1, LogLevel.Information, "Notification: sending notification from {From} to {To} with title {Title}.", EventName = "LogSentNotification")]
+  [LoggerMessage(1, LogLevel.Information, "Notifications: sending notification from {From} to {To} with title {Title}.", EventName = "LogSentNotification")]
   public static partial void LogSentNotification (ILogger logger, string from, string to, string title);
 }
