5b4acedc6248d42015b0a11cddf1711e39873f6a
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-22 14:34:30 +0000

    fix(sqlserver): fix tests connection string.

--- Simple.Infrastructure.SqlServer/Using.Tests.cs
+++ Simple.Infrastructure.SqlServer/Using.Tests.cs
@@ -8,7 +8,7 @@ namespace Simple.Infrastructure.SqlServer;
 public partial class SqlServerTests
 {
   static readonly SqlServerOptions SqlServerOptions = new SqlServerOptions() {DbName = "agenda-tests", AdminName = "sa", AdminPassword = "admin.P@ssw0rd", UserName = "sqluser", UserPassword = "sqluser.P@ssw0rd"};
-  static readonly string AgendaConnString = CreateSqlConnectionString(SqlServerOptions.ContainerName, SqlServerOptions.UserName, SqlServerOptions.UserPassword, SqlServerOptions.DbName);
+  static readonly string AgendaConnString = CreateSqlConnectionString(SqlServerOptions.DbName, SqlServerOptions.UserName, SqlServerOptions.UserPassword, SqlServerOptions.ContainerName);
 
   [AssemblyInitialize]
   public static void InitializeSqlServer (TestContext _)
