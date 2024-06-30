88171765cda0bd668a5d6a05168c97572bc9ab06
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-30 10:51:23 +0000

    change(queue): reimplement dequeue message error handling.

--- Simple.Infrastructure.Queue/MessageQueue/Dequeueing.cs
+++ Simple.Infrastructure.Queue/MessageQueue/Dequeueing.cs
@@ -23,22 +23,25 @@ partial class QueueFuncs
       try {
         message = await DequeueMessage(queue, cancellationToken);
 
-        LogHandlingMessage(logger, GetMessageId(message), GetMessageType(message), GetMessageCorrelationId(message));
+        LogHandleMessage(logger, GetMessageId(message), GetMessageType(message), GetMessageCorrelationId(message));
         await handleMessage(message);
       }
-      catch (OperationCanceledException) {
-        LogEndConsumingMessages(logger);
-        return;
-      }
-      catch (Exception ex) {
+      catch (OperationCanceledException) { break; }
+      catch (Exception exception) {
+        var messageId = GetMessageId(message);
+        var messageType = GetMessageType(message);
+        var correlationId = GetMessageCorrelationId(message);
         try {
-          LogHandledMessageError(logger, GetMessageId(message), GetMessageType(message), GetMessageCorrelationId(message), ex.Message, ex.StackTrace);
-          await handleMessageError(message, ex);
+          LogHandleMessageError(logger, messageId, messageType, correlationId, exception.Message, exception.StackTrace);
+          await handleMessageError(message, exception);
         }
-        catch(Exception innerEx) {
-          LogHandlingMessageError(logger, GetMessageId(message), GetMessageType(message), GetMessageCorrelationId(message), innerEx.Message, innerEx.StackTrace);
+        catch(Exception innerException) {
+          LogHandleInnerMessageError(logger, messageId, messageType, correlationId, innerException.Message, innerException.StackTrace);
+          if (innerException.InnerException is not null)
+            LogHandleInnerMessageError(logger, messageId, messageType, correlationId, innerException.InnerException.Message, innerException.InnerException.StackTrace);
         }
       }
     }
+    LogEndConsumingMessages(logger);
   }
 }
--- Simple.Infrastructure.Queue/QueueLoggers/Logging.cs
+++ Simple.Infrastructure.Queue/QueueLoggers/Logging.cs
@@ -5,8 +5,8 @@ namespace Simple.Infrastructure.Queue;
 
 partial class QueueFuncs
 {
-  [LoggerMessage(1, LogLevel.Information, "Queue: handling message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]", EventName = "LogHandlingMessage")]
-  public static partial void LogHandlingMessage (ILogger logger, Guid? messageId, string? messageType, string? correlationId);
+  [LoggerMessage(1, LogLevel.Information, "Queue: handle message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]", EventName = "LogHandleMessage")]
+  public static partial void LogHandleMessage (ILogger logger, Guid? messageId, string? messageType, string? correlationId);
 
   [LoggerMessage(2, LogLevel.Information, "Queue: start consuming messages.", EventName = "LogStartConsumingMessages")]
   public static partial void LogStartConsumingMessages (ILogger logger);
@@ -14,9 +14,9 @@ partial class QueueFuncs
   [LoggerMessage(3, LogLevel.Information, "Queue: end consuming messages.", EventName = "LogEndConsumingMessages")]
   public static partial void LogEndConsumingMessages (ILogger logger);
 
-  [LoggerMessage(11, LogLevel.Error, "Queue: handled error [{Error}] for message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]\n{StackTrace}", EventName = "LogHandlingMessageError")]
-  public static partial void LogHandledMessageError (ILogger logger, Guid? messageId, string? messageType, string? correlationId, string error, string? stackTrace);
+  [LoggerMessage(11, LogLevel.Error, "Queue: handle error [{Error}] for message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]\n{StackTrace}", EventName = "LogHandleMessageError")]
+  public static partial void LogHandleMessageError (ILogger logger, Guid? messageId, string? messageType, string? correlationId, string error, string? stackTrace);
 
-  [LoggerMessage(12, LogLevel.Error, "Queue: handling error [{Error}] for message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]\n{StackTrace}", EventName = "LogConsumingMessageError")]
-  public static partial void LogHandlingMessageError (ILogger logger, Guid? messageId, string? messageType, string? correlationId, string error, string? stackTrace);
+  [LoggerMessage(12, LogLevel.Error, "Queue: handle inner error [{Error}] for message with id [{MessageId}] and type [{MessageType}]. [{correlationId}]\n{StackTrace}", EventName = "LogHandleErrorMessageError")]
+  public static partial void LogHandleInnerMessageError (ILogger logger, Guid? messageId, string? messageType, string? correlationId, string error, string? stackTrace);
 }
