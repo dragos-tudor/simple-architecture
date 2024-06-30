6300edeea3d080c0e6c6041ece4f7dba6271d547
Dragos Tudor <dragos.tudor@gmail.com>
2024-06-30 10:52:54 +0000

    refactor(integrations): hide jobs initialization inside job scheduler integration.

--- Simple.Infrastructure.Integrations/Integrations/Integrating.JobScheduler.cs
+++ Simple.Infrastructure.Integrations/Integrations/Integrating.JobScheduler.cs
@@ -5,10 +5,11 @@ namespace Simple.Infrastructure.Integrations;
 
 partial class IntegrationsFuncs
 {
-  public static IDisposable IntegrateJobScheduler (IEnumerable<Job> jobs, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory)
+  public static IDisposable IntegrateJobScheduler (ServerIntegrations serverIntegrations, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default)
   {
     var jobSchedulerOptions = GetConfigurationOptions<JobSchedulerOptions>(configuration);
     var jobSchedulerLogger = loggerFactory.CreateLogger(nameof(JobScheduler.JobSchedulerFuncs));
+    var jobs = ResolveResumeMessagesJobs(serverIntegrations, configuration, TimeProvider.System, loggerFactory, cancellationToken);
 
     return RunJobs(jobs, jobSchedulerOptions, timeProvider, jobSchedulerLogger);
   }
--- Simple.Infrastructure.Integrations/MessageJobs/Resolving.cs
+++ Simple.Infrastructure.Integrations/MessageJobs/Resolving.cs
@@ -1,27 +1,29 @@
 
-namespace Simple.App;
+using Microsoft.Extensions.Configuration;
 
-partial class AppFuncs
+namespace Simple.Infrastructure.Integrations;
+
+partial class IntegrationsFuncs
 {
-  internal static IEnumerable<Job> ResolveResumeMessagesJobs (ServerIntegrations serverIntegrations, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default) => [
+  public static IEnumerable<Job> ResolveResumeMessagesJobs (ServerIntegrations serverIntegrations, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default) => [
     ResolveResumeMessagesMongoJob(serverIntegrations.MongoIntegration, configuration, timeProvider, loggerFactory, cancellationToken),
     ResolveResumeMessagesSqlJob(serverIntegrations.SqlIntegration, configuration, timeProvider, loggerFactory, cancellationToken)
   ];
 
-  internal static Job ResolveResumeMessagesMongoJob (MongoIntegration mongoIntegration, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default)
+  public static Job ResolveResumeMessagesMongoJob (MongoIntegration mongoIntegration, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default)
   {
     var (mongoDatabase, mongoMessageQueue) = mongoIntegration;
     var resumeMessagesJob = GetConfigurationOptions<ResumeMessagesJob>(configuration);
-    var jobSchedulerLogger = loggerFactory.CreateLogger(nameof(Infrastructure.JobScheduler.JobSchedulerFuncs));
+    var jobSchedulerLogger = loggerFactory.CreateLogger(nameof(JobScheduler.JobSchedulerFuncs));
 
     return resumeMessagesJob with { JobAction = () => ResumeMessagesMongoAsync(mongoDatabase, mongoMessageQueue, timeProvider, resumeMessagesJob.ResumeMessagesOptions, jobSchedulerLogger, cancellationToken) };
   }
 
-  internal static Job ResolveResumeMessagesSqlJob (SqlIntegration sqlIntegration, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default)
+  public static Job ResolveResumeMessagesSqlJob (SqlIntegration sqlIntegration, IConfiguration configuration, TimeProvider timeProvider, ILoggerFactory loggerFactory, CancellationToken cancellationToken = default)
   {
     var (sqlContextFactory, sqlMessageQueue) = sqlIntegration;
     var resumeMessagesJob = GetConfigurationOptions<ResumeMessagesJob>(configuration);
-    var jobSchedulerLogger = loggerFactory.CreateLogger(nameof(Infrastructure.JobScheduler.JobSchedulerFuncs));
+    var jobSchedulerLogger = loggerFactory.CreateLogger(nameof(JobScheduler.JobSchedulerFuncs));
 
     return resumeMessagesJob with { JobAction = () => ResumeMessagesSqlAsync(sqlContextFactory, sqlMessageQueue, timeProvider, resumeMessagesJob.ResumeMessagesOptions, jobSchedulerLogger, cancellationToken) };
   }
